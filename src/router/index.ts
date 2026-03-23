35a2c327f83c74a6f53531591c2c57ccd01eb74f
zhouyinkui <zhouyinkui233@163.com>
2026-03-23 13:42:48 +0800

    删除部分skill

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      redirect: '/login'
+    },
+    {
       path: '/login',
       name: 'login',
       component: () => import('../views/login/LoginPage.vue')
